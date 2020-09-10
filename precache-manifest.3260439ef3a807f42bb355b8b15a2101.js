8a9084fcd721ec6e45c257a26a325202df0e72ea
CCBaxter84 <troyceclark@gmail.com>
2020-09-09 18:27:42 -0600

    Updates

---
+++ precache-manifest.3260439ef3a807f42bb355b8b15a2101.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14bdfc2b4ac6c7ae7beb69828f5678d9",
+    "url": "/US-History-Quiz-React/index.html"
+  },
+  {
+    "revision": "ef61be4d9fc9018144a7",
+    "url": "/US-History-Quiz-React/static/css/main.14577ae5.chunk.css"
+  },
+  {
+    "revision": "964a9feb0bf4b539f10e",
+    "url": "/US-History-Quiz-React/static/js/2.5fe63647.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/US-History-Quiz-React/static/js/2.5fe63647.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef61be4d9fc9018144a7",
+    "url": "/US-History-Quiz-React/static/js/main.d2961514.chunk.js"
+  },
+  {
+    "revision": "ccaaa7f99f93ff021ce9",
+    "url": "/US-History-Quiz-React/static/js/runtime-main.6be29f90.js"
+  }
+]);
